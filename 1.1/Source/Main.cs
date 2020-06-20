608b41247cd64fdbfc5a629e41da4d8fd937e3f1
Arvkus <arvydasv201@gmail.com>
2020-06-20 11:23:14 +0300

    init

---
+++ 1.1/Source/Main.cs
@@ -0,0 +1,44 @@
+﻿// ----------------------------------------------------------------------
+// These are basic usings. Always let them be here.
+// ----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+// ----------------------------------------------------------------------
+// These are RimWorld-specific usings. Activate/Deactivate what you need:
+// ----------------------------------------------------------------------
+using UnityEngine;         // Always needed
+using Verse;               // RimWorld universal objects are here (like 'Building')
+using Verse.AI;            // Needed when you do something with the AI
+using Verse.AI.Group;
+using Verse.Sound;         // Needed when you do something with Sound
+using Verse.Noise;         // Needed when you do something with Noises
+using RimWorld;            // RimWorld specific functions are found here (like 'Building_Battery')
+using RimWorld.Planet;     // RimWorld specific functions for world creation
+
+
+namespace Template
+{
+
+    [DefOf]
+    public class TemplateDefOf
+    {
+        public static LetterDef success_letter;
+    }
+
+    public class MyMapComponent : MapComponent
+    {
+		public MyMapComponent(Map map) : base(map)
+        {
+		}
+
+        public  override  void FinalizeInit()
+		{
+            Messages.Message("Success", null, MessageTypeDefOf.PositiveEvent);
+            Find.LetterStack.ReceiveLetter("Success", TemplateDefOf.success_letter.description, TemplateDefOf.success_letter, null);  
+		}
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Rimworld mod template for Visual Studio Code
+## Rimworld version 1.1.2552 (64bit)
+Command to build dll is `dotnet build source`
+

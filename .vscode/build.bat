56bbdf097386e2e63d3b76078557c04c3f14f335
Arvkus <arvydasv201@gmail.com>
2020-07-28 12:03:00 +0300

    relocated intermediate files

--- .vscode/build.bat
+++ .vscode/build.bat
@@ -4,7 +4,7 @@ REM remove unnecessary assemblies
 DEL .\*\Assemblies\*.*
 
 REM build dll
-dotnet build Source
+dotnet build Source --no-restore
 
-REM remove obj folder for less clutter
+REM remove nuget dependencies folder for less clutter in case it's generated
 RMDIR /s /q Source\obj
